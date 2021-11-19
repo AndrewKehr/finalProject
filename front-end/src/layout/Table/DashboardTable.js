16d9e821f11e1400fb69af165d474d9067ed75cd
unknown <andrew.kehr@gmail.com>
2021-11-19 12:19:23 -0600

    I promise, this is the last border update

--- front-end/src/layout/Table/DashboardTable.js
+++ front-end/src/layout/Table/DashboardTable.js
@@ -41,7 +41,7 @@ function DashboardTable({
   }
 
   return (
-    <main className="border-left">
+    <main>
       <div className="mb-3 container-fluid">
         <h4 className="mb-0 text-center">Tables</h4>
       </div>
