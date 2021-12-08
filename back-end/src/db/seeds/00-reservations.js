1543d660046c4e9bc96d4a486de5affcc62bdce1
Andrew Kehr <85807215+AndrewKehr@users.noreply.github.com>
2021-12-08 14:00:53 -0600

    Delete back-end directory

--- back-end/src/db/seeds/00-reservations.js
+++
@@ -1,7 +0,0 @@
-const reservationsData = require("./00-reservations.json");
-console.log(reservationsData);
-exports.seed = function (knex) {
-  return knex
-    .raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
-    .then(() => knex("reservations").insert(reservationsData));
-};
--- back-end/src/db/seeds/01-tables.js
+++
@@ -1,7 +0,0 @@
-const tablesData = require("./01-tables.json");
-console.log(tablesData);
-exports.seed = function (knex) {
-  return knex
-    .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
-    .then(() => knex("tables").insert(tablesData));
-};
